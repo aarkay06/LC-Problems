c2eb4d67e94bc7ad393e155b47a16271f365d87f
AarKay <88158135+aarkay06@users.noreply.github.com>
2026-02-01 13:18:25 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (33.22%) - LeetSync

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,48 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    int search(vector<int> &nums, int target)
-    {
-        int sortAt = 0;
-        for (int i = 1; i < nums.size(); i++)
-        {
-            if (nums[i] < nums[i - 1])
-            {
-                sortAt = i;
-                break;
-            }
-        }
-
-        cout << "rotated at: " << sortAt << endl;
-
-        reverse(nums.begin(), nums.end());
-        reverse(nums.begin(), nums.begin() + nums.size() - sortAt);
-        reverse(nums.begin() + nums.size() - sortAt, nums.end());
-
-        for (int x : nums)
-        {
-            cout << x << " ";
-        }
+    int search(vector<int>& nums, int t) {
 
         int start = 0;
         int end = nums.size() - 1;
+        int last = nums[nums.size() - 1];
 
-        while (start <= end)
-        {
+        while (start <= end) {
             int mid = (start + end) / 2;
-            cout << "\nmid: " << mid << endl;
-            if (target == nums[mid])
-            {
-                return (mid + sortAt) % (nums.size());
+
+
+            if(nums[mid] == t) return mid;
+            // subarray 1
+            if (nums[mid] >= nums[end]) {
+                if (t > nums[mid] || t < nums[start]) {
+                    start = mid + 1;
+                } else  {
+                    end = mid - 1;
+                } 
             }
-            else if (target > nums[mid])
-                start = mid + 1;
-            else
-                end = mid - 1;
+            // subarray 2
+            else if (nums[mid] < nums[end]) {
+                if (t < nums[mid] || t > nums[end]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } 
         }
         return -1;
     }
 };
+
