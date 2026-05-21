0d5bbfd5d235a86fcd96fd25780252df5ef195a3
AarKay <88158135+aarkay06@users.noreply.github.com>
2026-05-22 01:25:45 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (35.87%) - LeetSync

---
+++ count-good-numbers/count-good-numbers.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+long long binExpo(long long a, long long b, long long mod)
+{
+    long long result = 1;
+
+    a %= mod;
+
+    while (b > 0)
+    {
+        // if b is odd
+        if (b & 1)
+        {
+            result = (result * a) % mod;
+        }
+
+        a = (a * a) % mod;
+
+        b >>= 1; // divide b by 2
+    }
+
+    return result;
+}
+    int countGoodNumbers(long long n) {
+        long long count = 1;
+        int m = 1e9 + 7;
+        if (n == 0)
+            return count;
+        if (n >= 1) {
+                count = (count % m * binExpo(5, (n+1)/2, m) % m);
+        }
+        if (n >= 2) {
+                 count = (count % m * binExpo(4, (n)/2, m) % m);
+       
+        }
+        return count;
+    }
+};
