0fbb85d2f6e2f822f20aeeff3c852b42f19c834d
AarKay <88158135+aarkay06@users.noreply.github.com>
2026-06-07 00:14:33 +0530

    Time: 29 ms (41.38%) | Memory: 16 MB (96.09%) - LeetSync

--- number-of-islands/number-of-islands.cpp
+++ number-of-islands/number-of-islands.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-    void bfs(int m, int n, vector<vector<char>>& grid,vector<vector<bool>>& visited) {
-        visited[m][n] = 1;
+   void bfs(int i, int j,vector<vector<char>>& grid){
+        if(i < 0 || i >= grid.size() || j < 0 || j>= grid[0].size() || grid[i][j] == '0') 
+        return;
 
-        if (m > 0 && !visited[m - 1][n] && grid[m - 1][n] == '1')
-            bfs(m - 1, n, grid, visited);
+        grid[i][j] = '0';
 
-        if (m < grid.size() - 1 && !visited[m + 1][n] && grid[m + 1][n] == '1')
-            bfs(m + 1, n, grid, visited);
-
-        if (n > 0 && !visited[m][n - 1] && grid[m][n - 1] == '1')
-            bfs(m, n - 1, grid, visited);
-
-        if (n < grid[0].size() - 1 && !visited[m][n + 1] &&
-            grid[m][n + 1] == '1')
-            bfs(m, n + 1, grid, visited);
-    }
+        bfs(i-1, j, grid);
+        bfs(i+1, j, grid);
+        bfs(i, j-1, grid);
+        bfs(i, j+1, grid);
+        
+   }
 
     int numIslands(vector<vector<char>>& grid) {
-        vector<vector<bool>> visited(grid.size(),vector<bool>(grid[0].size(), 0));
         int count = 0;
 
-        for (int i = 0; i < visited.size(); i++) {
-            for (int j = 0; j < visited[i].size(); j++) {
-                if (!visited[i][j] && grid[i][j] == '1') {
-                    bfs(i, j, grid, visited);
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
+                if (grid[i][j] == '1') {
+                    bfs(i, j, grid);
                     count++;
                 }
             }
