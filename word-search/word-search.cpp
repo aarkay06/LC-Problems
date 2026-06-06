05b09fac2d35a648884581f79efab61ff87c7471
AarKay <88158135+aarkay06@users.noreply.github.com>
2026-06-07 03:22:12 +0530

    Time: 211 ms (64.58%) | Memory: 10.3 MB (92.73%) - LeetSync

---
+++ word-search/word-search.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool bfs(int i, int j, vector<vector<char>>& board, int index,
+             string& word) {
+
+        if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() ||
+            board[i][j] == '^' || board[i][j] != word[index])
+            return false;
+
+        char a = board[i][j];
+
+        if (index == word.size() - 1)
+            return true;
+        board[i][j] = '^';
+        if (bfs(i - 1, j, board, index + 1, word) +
+            bfs(i + 1, j, board, index + 1, word) +
+            bfs(i, j - 1, board, index + 1, word) +
+            bfs(i, j + 1, board, index + 1, word))
+            return true;
+
+        board[i][j] = a;
+        return false;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[i].size(); j++) {
+                if (board[i][j] == word[0]) {
+                    if (bfs(i, j, board, 0, word))
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+};
