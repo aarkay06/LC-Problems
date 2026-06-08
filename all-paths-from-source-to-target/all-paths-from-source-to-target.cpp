5472ea2ee84f5876be5752ed71047b6436c076b4
AarKay <88158135+aarkay06@users.noreply.github.com>
2026-06-08 18:33:26 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (59.37%) - LeetSync

--- all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
+++ all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    void dfs(int node, int n, vector<vector<int>>& graph, vector<int> arr,
+    void dfs(int node, int n, vector<vector<int>>& graph, vector<int>& arr,
              vector<vector<int>>& res) {
 
         if (node == n - 1) {
             arr.push_back(node);
             res.push_back(arr);
+            arr.pop_back();
             return;
         }
-       else if (graph[node].size() == 0 ) {
-            
-                return;
-        }
+
         arr.push_back(node);
         for (int i = 0; i < graph[node].size(); i++) {
             dfs(graph[node][i], n, graph, arr, res);
         }
+        arr.pop_back();
     }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         vector<vector<int>> res;
-        dfs(0, graph.size(), graph, {}, res);
+        vector<int> arr;
+        dfs(0, graph.size(), graph,arr, res);
         return res;
     }
 };
