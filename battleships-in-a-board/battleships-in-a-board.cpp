9617321b1c86958923bcdff0fd7c4c59a1e50a30
AarKay <88158135+aarkay06@users.noreply.github.com>
2026-06-10 01:27:23 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (83.71%) - LeetSync

---
+++ battleships-in-a-board/battleships-in-a-board.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int dfs(int i, int j, vector<vector<char>>& board) {
+        if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() ||
+            board[i][j] == '.') {
+            return 1;
+        }
+
+        board[i][j] = '.';
+
+        dfs(i - 1, j, board);
+        dfs(i + 1, j, board);
+        dfs(i, j - 1, board);
+        dfs(i, j + 1, board);
+        return 1;
+    }
+
+    int countBattleships(vector<vector<char>>& board) {
+        int count = 0;
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[i].size(); j++) {
+                if (board[i][j] == 'X')
+                    count += dfs(i, j, board);
+            }
+        }
+        return count;
+    }
+};
