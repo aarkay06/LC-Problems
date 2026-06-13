7b179fd0ad5195a6e0112ff70369c61694d4a19a
AarKay <88158135+aarkay06@users.noreply.github.com>
2026-06-13 22:22:00 +0530

    Time: 14 ms (5.10%) | Memory: 19.3 MB (5.26%) - LeetSync

---
+++ is-graph-bipartite/is-graph-bipartite.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    bool bfs(int node,
+             vector<vector<int>>& graph, queue<pair<int, int>>& q, vector<int> &visited) {
+
+        q.push({node, 1});
+        visited[node] = 1;
+
+        while (!q.empty()) {
+            auto [node, s] = q.front();
+            q.pop();
+            if (s == 1) {
+                if (visited[node] == 2)
+                    return false;
+                visited[node] = 1;
+            }
+
+            if (s == 2) {
+              if (visited[node] == 1)
+                    return false;
+                visited[node] = 2;
+            }
+
+            // if(graph[node].size() == 0) return false;
+
+            for (int i = 0; i < graph[node].size(); i++) {
+                if (s == 1 && !visited[graph[node][i]]) {
+                    q.push({graph[node][i], 2});
+                } else if (s == 2 && !visited[graph[node][i]]) {
+                    q.push({graph[node][i], 1});
+                }
+            }
+        }
+
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        vector<int> visited(graph.size());
+        queue<pair<int, int>> q;
+        for (int i = 0; i < graph.size(); i++) {
+            if (!visited[i] && graph[i].size() > 0) {
+                bool a = bfs(i,graph, q, visited);
+                if (a == false)
+                    return false;
+            }
+        }
+        return true;
+    }
+};
