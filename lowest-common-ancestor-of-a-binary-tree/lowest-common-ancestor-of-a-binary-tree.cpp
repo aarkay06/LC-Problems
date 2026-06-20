cd93188c06aafe1244fd6a480541372c14ec11ef
AarKay <88158135+aarkay06@users.noreply.github.com>
2026-06-21 00:32:30 +0530

    Time: 47 ms (32.24%) | Memory: 41.8 MB (29.66%) - LeetSync

--- lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,55 +9,23 @@
  */
 class Solution {
 public:
-    vector<int> pTree;
-    vector<int> qTree;
-     TreeNode* ansNode;
 
-    void traverse(TreeNode* root, TreeNode* r, vector<int>& treeArr, int in) {
-        if (!root)
-            return;
-        treeArr.push_back(root->val);
-        if (root->val == r->val) {
-            if (in == 0)
-                pTree = treeArr;
-            if (in == 1)
-                qTree = treeArr;
-            return;
-        }
-        traverse(root->left, r, treeArr, in);
-        traverse(root->right, r, treeArr, in);
-        treeArr.pop_back();
-    }
+    TreeNode* traverse(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root || root == p || root == q)
+            return root;
 
-   
-    void node(TreeNode* root, int val){
-         if (!root)
-            return;
-        if (root->val == val) {
-            ansNode =  root;
-            return;
-        }
-        node(root->left, val);
-        node(root->right, val);
+        root->left = traverse(root->left, p,q);
+        root->right = traverse(root->right, p,q);
+
+        if(root->left && root->right)
+            return root;
+        
+        return root->left ? root->left : root->right;
+
     }
 
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<int> arr;
-        traverse(root, p, arr, 0);
-        arr.clear();
-        traverse(root, q, arr, 1);
+          return  traverse(root, p,q );
 
-        int size1 = pTree.size();
-        int size2 = qTree.size();
-
-     
-        int ans;
-        for (int i = 0; i < min(size1, size2); i++) {
-            if (pTree[i] == qTree[i])
-                ans = pTree[i];
-        }
-
-node(root, ans);
-        return ansNode;
     }
 };
