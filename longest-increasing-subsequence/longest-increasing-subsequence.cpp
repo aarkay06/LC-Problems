37d7552f6053e9ac6d632594b1393a34ff885854
Raj Krishna <88158135+aarkay06@users.noreply.github.com>
2026-07-18 11:54:48 +0530

    Time: 64 ms (59.71%) | Memory: 14.2 MB (77.83%) - LeetSync

--- longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size(), vector<int>(nums.size(), -1)); 
-        return subsequence(0, -1, nums, dp);
-    }
-
-    int subsequence(int i, int prev, vector<int>& nums,  vector<vector<int>> &dp) {
-        if(i == nums.size()) return 0;
-        if(dp[i][prev+1] != -1) return dp[i][prev+1];
-        // pick
-        int pick =0, notpick=0;
-        if (prev == -1 || nums[i] > nums[prev]) {
-            pick = 1 + subsequence(i + 1, i, nums, dp);
+        vector<int> dp(nums.size()); 
+        dp[0] = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            dp[i]=1;
+              for (int j = 0; j < i ; j++) {
+                    if(nums[j] < nums[i] && dp[j]+1 > dp[i]){
+                        dp[i] = dp[j] +1;
+                    }
+              }
         }
-        // not pick
-        notpick = 0 + subsequence(i + 1, prev, nums, dp);
 
-        return dp[i][prev+1] = max(pick, notpick);
+        return *max_element(dp.begin(), dp.end());
     }
+
+
 };
