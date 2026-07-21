24eba6fdcb317bd4ba804c60e624bd379217590f
Raj Krishna <88158135+aarkay06@users.noreply.github.com>
2026-07-21 11:43:17 +0530

    Time: 24 ms (15.20%) | Memory: 38.6 MB (25.95%) - LeetSync

--- binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,36 +1,24 @@
 
-class Solution
-{
+class Solution {
 public:
-    int numSubarraysWithSum(vector<int> &nums, int goal)
-    {
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
 
-        int count = 0;
-        for (int i = 1; i < nums.size(); i++)
-        {
+        for (int i = 1; i < nums.size(); i++) {
             nums[i] = nums[i - 1] + nums[i];
         }
 
-        for (int i = (goal - 1) > 0 ? goal - 1 : 0; i < nums.size(); i++)
-        {
-            // arr[i] - arr[j] = goal
-            if (nums[i] < goal)
-                continue;
-
-            int j = (nums[i] - goal - 1 >= 0) ? nums[i] - goal - 1 : 0;
-
+        int count = 0;
+        unordered_map<int, int> m;
+        for (int i = 0; i < nums.size(); i++) {
             if (nums[i] == goal)
                 count++;
-            while (j < i)
-            {
 
-                if (nums[i] - nums[j] == goal)
-                    count++;
-                if (nums[i] - nums[j] < goal)
-                    break;
-                j++;
-            }
+            if (m.contains(nums[i] - goal))
+                count += m[nums[i] - goal];
+
+            m[nums[i]]++;
         }
+
         return count;
     }
 };
