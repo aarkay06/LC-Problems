32041efe78413ff7cc9378c78e8b04f40b7a7eb1
Raj Krishna <88158135+aarkay06@users.noreply.github.com>
2026-07-28 00:49:11 +0530

    Time: 0 ms (100.00%) | Memory: 185.3 MB (40.91%) - LeetSync

--- swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
+++ swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
@@ -10,27 +10,47 @@
  */
 class Solution {
 public:
-    ListNode* n1 = nullptr;
-    ListNode* n2 = nullptr;
-    int size = 0;
-    void recursion(ListNode* head, int k, int i) {
-        if (head == nullptr)
-            return;
-        else {
-            size++;
-            if (i == k)
-                n1 = head;
-            recursion(head->next, k, i + 1);
-        }
-        if (size - i + 1 == k)
-            n2 = head;
-    }
+    // two pointers
+    // when first pointer is at 0 (1st from left), second pointer at k-1 (kth
+    // from start) when second pointer at end, first pointer at kth from last
+    //  1 2 3 4 5 6 7 8
+    //  s . . . . . f
+    //    s . . . . . f
+
+    // 1 2 3 4 5 6 7 8
+    // s . . f
+    //         s . . f
+    // initially s at 0, move both until f at end, s will be at answer
+    // two pointers but their distance has to be the same, both will move 1 node
+    // at a time
 
     ListNode* swapNodes(ListNode* head, int k) {
-        recursion(head, k, 1);
-        int temp = n1->val;
-        n1->val = n2->val;
-        n2->val = temp;
+        ListNode* n1;
+        ListNode* n2;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        // k-1 cause we are moving ahead k-1 times
+        // basically if k = 3
+        // we have to move AHEAD from the head node only twice
+
+        for (int i = 0; i < k - 1; i++) {
+            fast = fast->next;
+        }
+
+        n1 = fast;
+        while (fast->next) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+
+        n2 = slow;
+
+        int temp = n2->val;
+        n2->val = n1->val;
+        n1->val = temp;
+
         return head;
     }
 };
